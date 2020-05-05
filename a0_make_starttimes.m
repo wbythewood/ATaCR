6018344071dc01ff0ea24deb67c7f0a0e4d56d7d
wbythewood <whawley@seismo.berkeley.edu>
2020-05-04 21:01:54 -0400

    made changes to a0, added param file

--- a0_make_starttimes.m
+++ a0_make_starttimes.m
@@ -1,32 +1,27 @@
 % make_starttimes
 %
-% Make start time files for daily data downloads from list of event times in 
+% Make start time files for daily data downloads from list of event times in
 % format YYYYmmddhhMM
 %
-% J. Russell & H. Janiszewski 
+% J. Russell & H. Janiszewski
 % hjaniszewski@carnegiescience.edu
 % updated 11/19
 
-% Input eventfile
-evfile = 'NOISETC_CI/eventtimes_CItest.txt';
+% W.B. Hawley update to use parameter file
+% updated 02/20
 
-% Output dayfile
-dayfile = 'NOISETC_CI/starttimes_CItest.txt';
-
-% Number of days prior to event for calculation of spectra
-Ndays = 4;
+setup_parameter;
 
 % Load event list
-evlist = textread(evfile,'%s');
+evlist = textread(evFile,'%s');
 
-fid = fopen(dayfile,'w');
+fid = fopen(dayFile,'w');
 for iev = 1:length(evlist)
     evnum = datenum(evlist(iev),'yyyymmddHHMM');
     daynums = flip(evnum - [1:Ndays]);
     for iday = 1:length(daynums)
         day = datestr(daynums(iday),'yyyymmddHHMM');
         fprintf(fid,'%s\n',day);
-    end    
+    end
 end
 fclose(fid);
-
--- a1_download_data.m
+++ a1_download_data.m
@@ -4,74 +4,127 @@
 % transfer functions for tilt and compliance corrections and saves them as
 % matfiles (default is 24 hours of data in each file).
 
-% H. Janiszewski 
+% H. Janiszewski
 % hjaniszewski@carnegiescience.edu
 % updated 2/18
 
-clear;
+% W.B. Hawley updated to include paramter file
+% also download event data in the same script
+% updated 02/20
 
-javaaddpath('IRIS-WS-2.0.6.jar');
+clear; close all
+setup_parameter;
 
-startlist = 'NOISETC_CI/starttimes_CItest.txt'; % list of start times for data download
-datalength = 86400; % length of time series after each start time in seconds (default 86400, code not thoroughly tested for other values)
-
-download_networks = '7D'; % list of networks to download
-download_stations = textread('./NOISETC_CI/stalist.txt','%s'); % list of stations to download (* for all)
-
-% Channel Names
-chz_vec = 'BHZ'; % list of acceptable names for Z component
-ch1_vec = 'BH1'; % list of acceptable names for H1 component
-ch2_vec = 'BH2'; % list of acceptable names for H2 component
-chp_vec = 'BDH'; % list of acceptable names for P component
-
-datacache = 'NOISETC_CI/DATA/datacache_day'; % output folder for data
-
-%%%%% end user input parameters %%%%%
-
-if ~exist(datacache,'dir')
-    mkdir(datacache)
+% check if directories exist
+if ~exist(NoiseDataDir,'dir')
+    mkdir(NoiseDataDir)
+end
+if ~exist(EventDataDir,'dir')
+    mkdir(EventDataDir)
 end
 
-startlist = textread(startlist,'%s');
-chanlist = sprintf('%s,%s,%s,%s',chz_vec,ch1_vec,ch2_vec,chp_vec);
+% a few parameters that are similar across noise+event data
+chanlist = [chz_vec, ch1_vec, ch2_vec, chp_vec];
+chanlist = strjoin(chanlist,',');
+
+%% first download the noise data
+
+startlist = textread(dayFile,'%s');
+
 
 for id = 1:length(startlist)
    eventid = cell2mat(startlist(id));
-   disp(sprintf('Start Time: %s',eventid));
-   otime = datenum(eventid,'yyyymmddHHMM');
+   fprintf('Start Time: %s\n',eventid);
+   otime = datenum(eventid,'yyyymmddHHMM'); 
    starttime = datestr(otime,'yyyy-mm-dd HH:MM:SS');
-   endtime = datestr(otime+datalength/3600/24,'yyyy-mm-dd HH:MM:SS');
+   endtime = datestr(otime+NoiseDataLength/3600/24,'yyyy-mm-dd HH:MM:SS');
+
+   stations_info = irisFetch.Stations('channel',NetworkName,StationNames,'*',chz_vec,'startTime',starttime,'endTime',endtime);
+   disp([stations_info])
+
+   for ista =1:length(stations_info)
+       error = 0;
+       disp([stations_info(ista)])
+       stnm = stations_info(ista).StationCode;
+       network = stations_info(ista).NetworkCode;
+       if ~exist(fullfile(NoiseDataDir,network),'dir')
+           mkdir(fullfile(NoiseDataDir,network));
+       end
+       if ~exist(fullfile(NoiseDataDir,network,stnm),'dir')
+           mkdir(fullfile(NoiseDataDir,network,stnm));
+       end
+       sta_filename = fullfile(NoiseDataDir,network,stnm,[eventid,'_',network,'_',stnm,'.mat']);
+%        if exist(sta_filename,'file')
+%            disp(['Exist: ',sta_filename,', Skip!']);
+%            continue;
+%        end
+       disp(['Downloading station: ',stnm,' From:',starttime,' To:',endtime]);
+		   try
+            traces_day = irisFetch.Traces(network,stnm,'*',chanlist,starttime,endtime,'includePZ');
+			 save(sta_filename,'traces_day');
+		   catch e
+            e.message;
+            error = 1;
+       end
+        if error ==1
+            try % to try and get around the missing zeros for some pressure components
+                %traces_day = irisFetch.Traces(network,stnm,'*',chanlist,starttime,endtime);
+                %save(sta_filename,'traces_day');
+            catch e
+                e.message;
+                continue;
+            end
+        end
+    end
+
+end
+%% Now the event data
+
+startlist = textread(evFile,'%s');
+
+for id = 1:length(startlist)
+   eventid = cell2mat(startlist(id));
+   fprintf('Start Time: %s\n',eventid);
+   if length(eventid) == 12
+        otime = datenum(eventid,'yyyymmddHHMM'); % look at this when I get back
+   elseif length(eventid) == 14
+       otime = datenum(eventid,'yyyymmddHHMMSS');
+       eventid = eventid(1:12); % for naming purposes only, start time will still be saved to the second in traces file
+   end
+   starttime = datestr(otime,'yyyy-mm-dd HH:MM:SS');
+   endtime = datestr(otime+EventDataLength/3600/24,'yyyy-mm-dd HH:MM:SS');
    
-   stations_info = irisFetch.Stations('channel',download_networks,download_stations,'*',chz_vec,'startTime',starttime,'endTime',endtime);
+   stations_info = irisFetch.Stations('channel',NetworkName,StationNames,'*',chz_vec,'startTime',starttime,'endTime',endtime);
    
    
    for ista =1:length(stations_info)
+       disp([stations_info(ista)])
        error = 0;
        stnm = stations_info(ista).StationCode;
        network = stations_info(ista).NetworkCode;
-       if ~exist(fullfile(datacache,network),'dir')
-           mkdir(fullfile(datacache,network));
+       if ~exist(fullfile(EventDataDir,eventid),'dir')
+           mkdir(fullfile(EventDataDir,eventid));
        end
-       if ~exist(fullfile(datacache,network,stnm),'dir')
-           mkdir(fullfile(datacache,network,stnm));
-       end
-       sta_filename = fullfile(datacache,network,stnm,[eventid,'_',network,'_',stnm,'.mat']);
-       if exist(sta_filename,'file')
-           disp(['Exist: ',sta_filename,', Skip!']);
-           continue;
-       end
+       sta_filename = fullfile(EventDataDir,eventid,[eventid,'_',network,'_',stnm,'.mat']);
+%        if exist(sta_filename,'file')
+%            disp(['Exist: ',sta_filename,', Skip!']);
+%            continue;
+%        end
        disp(['Downloading station: ',stnm,' From:',starttime,' To:',endtime]);
 		try
-            traces_day = irisFetch.Traces(network,stnm,'*',chanlist,starttime,endtime,'includePZ');
-			save(sta_filename,'traces_day');
+            disp([network,stnm,chanlist,starttime,endtime])
+            
+            traces = irisFetch.Traces(network,stnm,'*',chanlist,starttime,endtime,'includePZ');
+            disp(sta_filename)
+            save(sta_filename,'traces');
 		catch e
             e.message;
             error = 1;
         end
         if error ==1
             try % to try and get around the missing zeros for some pressure components                
-                traces_day = irisFetch.Traces(network,stnm,'*',chanlist,starttime,endtime);
-                save(sta_filename,'traces_day');
+                traces = irisFetch.Traces(network,stnm,'*',chanlist,starttime,endtime);
+                save(sta_filename,'traces');
             catch e
                 e.message;
                 continue;
