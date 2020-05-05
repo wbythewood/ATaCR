8d5749393d8f1a0d3a6d26ad2f3f203c6d2f804a
wbythewood <whawley@seismo.berkeley.edu>
2020-05-04 21:17:08 -0400

    Add option to apply instrument response here. Also some cosmetic stuff.

--- b4_correctevent.m
+++ b4_correctevent.m
@@ -2,6 +2,7 @@
 % script for correcting event files for tilt and compliance noise
 
 clear all; close all
+setup_parameter;
 
 isfigure_sta = 1;
 isfigure_spectra = 1;
@@ -10,13 +11,13 @@ isoverwrite =1;
 
 T1 = 10; T2= 150; %filter period range for plotting seismic data
 
-inpath_event = 'NOISETC_CI/DATA/datacache_prepro/'; % path to earthquake data (data to be corrected)
+inpath_event = EventPreproDir; %'../../data/datacache_prepro/'; % path to earthquake data (data to be corrected)
 
 % input lists of stations with bad components, file doesn't have to exists
 % leave as default if no bad stations
-badstalist = 'NOISETC_CI/Bad_Z.txt';
-badhorzlist = 'NOISETC_CI/Bad_H.txt';
-badpreslist = 'NOISETC_CI/Bad_P.txt';
+badstalist = '../../Bad_Z.txt';
+badhorzlist = '../../Bad_H.txt';
+badpreslist = '../../Bad_P.txt';
 
 %% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% %%
 %% %%%%%%%%%%%%%%%%%%%%%%%%% DO NOT EDIT BELOW %%%%%%%%%%%%%%%%%%%%%%%%% %%
@@ -40,6 +41,7 @@ else
 end
 
 for itf = 1:length(TF_list)
+    disp([itf])
     TF_size(itf) = length(TF_list{itf});
 end
 [vec,TF_list_sort] = sort(TF_size,'descend');
@@ -85,7 +87,7 @@ for ie = 1:length(event_filename)
         idx2 = findstr(event_info_dir(ista).name,'.mat');
         
         network = event_info_dir(ista).name(idx1(1)+1:idx1(2)-1);
-        station = event_info_dir(ista).name(idx1(2)+1:idx2-1);
+        station = event_info_dir(ista).name(idx1(2)+1:idx2-1)
         
         netsta = [network,station];
         
@@ -185,87 +187,126 @@ for ie = 1:length(event_filename)
         
         ii = 1;
         for itf = 1:length(TF_list_sort)
-            TF_cal = TF_list(TF_list_sort(itf));
+            TF_cal = TF_list(TF_list_sort(itf)); % name of the TF we're applying now
             if TF_check(TF_list_sort(itf)) == 0 % check if TF calculated already
-                % check if rotational or not
-                if ~isempty(strfind(cell2mat(TF_cal),'H'))
-                if  tf_op == 2 % average doesn't make sense for this
-                    continue
-                end
-                if length(TF_cal{1}) == 2 % 1 component rotational TF, i.e. ZH
-                    [corrspec,corrtime,corrgood,label_list] = tfcomp1rotate_correctevent(TF_cal,TFs,transprop,spec_mat,NFFT,dt,npad0,npts,goodP,goodH,goodZ);
-                    TF_check(TF_list_sort(itf)) = 1;
-                    corrseis(ii).label = label_list{1};
-                    corrseis(ii).spectrum = corrspec;
-                    corrseis(ii).timeseries = corrtime;
-                    corrseis(ii).isgood = corrgood;
-                    ii = ii+1;
-                elseif length(TF_cal{1}) == 4 % ZP-H
-                    [corrspec1_2,corrspec1_32,corrtime1_2,corrtime1_32,corrgood1_2,corrgood1_32,...
-                        label_list] = tfcomp2rotate_correctevent(TF_cal,TFs,transprop,spec_mat,NFFT,dt,npad0,npts,goodP,goodH,goodZ);
-                    TF_check(TF_list_sort(itf)) = 1;
-                    corrseis(ii).label = label_list{1};
-                    corrseis(ii).spectrum = corrspec1_2;
-                    corrseis(ii).timeseries = corrtime1_2;
-                    corrseis(ii).isgood = corrgood1_2;
-                    if ~isempty(find(strcmp(label_list{1},TF_list)==1))
-                        TFidx = find(strcmp(label_list{1},TF_list)==1);
-                        TF_check(TFidx)=1;
+                % check if rotational or not, i.e., H max
+                if ~isempty(strfind(cell2mat(TF_cal),'H')) % if rotational
+                    if  tf_op == 2 % average doesn't make sense for this
+                        continue
                     end
-                    corrseis(ii+1).label = label_list{2};
-                    corrseis(ii+1).spectrum = corrspec1_32;
-                    corrseis(ii+1).timeseries = corrtime1_32;
-                    corrseis(ii+1).isgood = corrgood1_32;
-                    if ~isempty(find(strcmp(label_list{2},TF_list)==1))
-                        TFidx = find(strcmp(label_list{2},TF_list)==1);
-                        TF_check(TFidx)=1;
+                    if length(TF_cal{1}) == 2 % 1 component rotational TF, i.e. ZH
+                        [corrspec,corrtime,corrgood,label_list] = tfcomp1rotate_correctevent(TF_cal,TFs,transprop,spec_mat,NFFT,dt,npad0,npts,goodP,goodH,goodZ);
+                        TF_check(TF_list_sort(itf)) = 1;
+                        corrseis(ii).label = label_list{1};
+                        corrseis(ii).spectrum = corrspec;
+                        corrseis(ii).timeseries = corrtime;
+                        corrseis(ii).isgood = corrgood;
+                        ii = ii+1;
+                    elseif length(TF_cal{1}) == 4 % ZP-H
+                        [corrspec1_2,corrspec1_32,corrtime1_2,corrtime1_32,corrgood1_2,corrgood1_32,...
+                            label_list] = tfcomp2rotate_correctevent(TF_cal,TFs,transprop,spec_mat,NFFT,dt,npad0,npts,goodP,goodH,goodZ);
+                        TF_check(TF_list_sort(itf)) = 1;
+                        corrseis(ii).label = label_list{1};
+                        corrseis(ii).spectrum = corrspec1_2;
+                        corrseis(ii).timeseries = corrtime1_2;
+                        corrseis(ii).isgood = corrgood1_2;
+                        if ~isempty(find(strcmp(label_list{1},TF_list)==1))
+                            TFidx = find(strcmp(label_list{1},TF_list)==1);
+                            TF_check(TFidx)=1;
+                        end
+                        corrseis(ii+1).label = label_list{2};
+                        corrseis(ii+1).spectrum = corrspec1_32;
+                        corrseis(ii+1).timeseries = corrtime1_32;
+                        corrseis(ii+1).isgood = corrgood1_32;
+                        if ~isempty(find(strcmp(label_list{2},TF_list)==1))
+                            TFidx = find(strcmp(label_list{2},TF_list)==1);
+                            TF_check(TFidx)=1;
+                        end
+                        ii = ii+2;
                     end
-                    ii = ii+2;
-                end
-                
-                else % if component wise
-                if length(TF_cal{1}) == 2 % 1 component correction
-                    [corrspec,corrtime,corrgood,label_list] = tfcomp1_correctevent(TF_cal,TFs,spec_mat,NFFT,dt,npad0,npts,goodP,goodH,goodZ);
-                    TF_check(TF_list_sort(itf)) = 1;
-                    corrseis(ii).label = label_list{1};
-                    corrseis(ii).spectrum = corrspec;
-                    corrseis(ii).timeseries = corrtime;
-                    corrseis(ii).isgood = corrgood;
-                    ii = ii+1;
-                elseif length(TF_cal{1}) == 5 % 3 component correction
-                    [corrspec1_2,corrspec1_32,corrspec1_432,corrtime1_2,...
-                        corrtime1_32,corrtime1_432,corrgood1_2,corrgood1_32,corrgood1_432,label_list] =...
-                        tfcomp3_correctevent(TF_cal,TFs,spec_mat,NFFT,dt,npad0,npts,goodP,goodH,goodZ);
-                    TF_check(TF_list_sort(itf)) = 1;
-                    corrseis(ii).label = label_list{1};
-                    corrseis(ii).spectrum = corrspec1_2;
-                    corrseis(ii).timeseries = corrtime1_2;
-                    corrseis(ii).isgood = corrgood1_2;
-                    if ~isempty(find(strcmp(label_list{1},TF_list)==1))
-                        TFidx = find(strcmp(label_list{1},TF_list)==1);
-                        TF_check(TFidx)=1;
+
+                 else % if component wise, i.e., H1 and H2
+                    if length(TF_cal{1}) == 2 % 1 component correction
+                        [corrspec,corrtime,corrgood,label_list] = tfcomp1_correctevent(TF_cal,TFs,spec_mat,NFFT,dt,npad0,npts,goodP,goodH,goodZ);
+                        TF_check(TF_list_sort(itf)) = 1;
+                        corrseis(ii).label = label_list{1};
+                        corrseis(ii).spectrum = corrspec;
+                        corrseis(ii).timeseries = corrtime;
+                        corrseis(ii).isgood = corrgood;
+                        ii = ii+1;
+                    elseif length(TF_cal{1}) == 5 % 3 component correction, i.e., ZP-21
+                        [corrspec1_2,corrspec1_32,corrspec1_432,corrtime1_2,...
+                            corrtime1_32,corrtime1_432,corrgood1_2,corrgood1_32,corrgood1_432,label_list] =...
+                            tfcomp3_correctevent(TF_cal,TFs,spec_mat,NFFT,dt,npad0,npts,goodP,goodH,goodZ);
+                        TF_check(TF_list_sort(itf)) = 1;
+                        corrseis(ii).label = label_list{1}; % this is Z1 correction...
+                        corrseis(ii).spectrum = corrspec1_2;
+                        corrseis(ii).timeseries = corrtime1_2;
+                        corrseis(ii).isgood = corrgood1_2;
+                        if ~isempty(find(strcmp(label_list{1},TF_list)==1))
+                            TFidx = find(strcmp(label_list{1},TF_list)==1);
+                            TF_check(TFidx)=1;
+                        end
+                        corrseis(ii+1).label = label_list{2}; % Z2-1 correction
+                        corrseis(ii+1).spectrum = corrspec1_32;
+                        corrseis(ii+1).timeseries = corrtime1_32;
+                        corrseis(ii+1).isgood = corrgood1_32;
+                        if ~isempty(find(strcmp(label_list{2},TF_list)==1))
+                            TFidx = find(strcmp(label_list{2},TF_list)==1);
+                            TF_check(TFidx)=1;
+                        end
+                        corrseis(ii+2).label = label_list{3}; % and ZP-21 correction
+                        corrseis(ii+2).spectrum = corrspec1_432;
+                        corrseis(ii+2).timeseries = corrtime1_432;
+                        corrseis(ii+2).isgood = corrgood1_432;
+                        if ~isempty(find(strcmp(label_list{3},TF_list)==1))
+                            TFidx = find(strcmp(label_list{3},TF_list)==1);
+                            TF_check(TFidx)=1;
+                        end
+                        ii=ii+3;
                     end
-                    corrseis(ii+1).label = label_list{2};
-                    corrseis(ii+1).spectrum = corrspec1_32;
-                    corrseis(ii+1).timeseries = corrtime1_32;
-                    corrseis(ii+1).isgood = corrgood1_32;
-                    if ~isempty(find(strcmp(label_list{2},TF_list)==1))
-                        TFidx = find(strcmp(label_list{2},TF_list)==1);
-                        TF_check(TFidx)=1;
-                    end
-                    corrseis(ii+2).label = label_list{3};
-                    corrseis(ii+2).spectrum = corrspec1_432;
-                    corrseis(ii+2).timeseries = corrtime1_432;
-                    corrseis(ii+2).isgood = corrgood1_432;
-                    if ~isempty(find(strcmp(label_list{3},TF_list)==1))
-                        TFidx = find(strcmp(label_list{3},TF_list)==1);
-                        TF_check(TFidx)=1;
-                    end
-                    ii=ii+3;
-                end
                 end
             end
         end
+        
+        % wbh edits?try applying instrument response here
+        % first need pz from the z component raw data
+        % '/Users/whawley/Research/github/ATaCR/data/datacache/201201231604'
+        staFileName = sprintf('%s_%s_%s.mat',eventid,network,station);
+        load(fullfile(EventDataDir,eventid,staFileName))
+%         dataDirName = dir(fullfile(EventDataDir));
+%         %dateFileName = sprintf('%s%s/',EventDataDir,event_filename(fileLoop).name);
+%         dateFileName = sprintf('%s%s/',EventPreprocDir,event_filename(fileLoop).name);
+%         stationFileNames = dir(fullfile(inpath,['*.mat']));
+%         if isempty(stationFileNames)
+%             continue
+%         end
+%         for ista = 1:length(stationFileNames)
+%             if isempty(findstr([NetworkName,'_',station],stationFileNames(ista).name))
+%                 continue
+%             end
+%             load(fullfile(EventDataDir,'/',dataDirName(fileLoop).name,'/',stationFileNames(ista).name));
+% 
+%         end
+        zIndex = find(contains({traces.channel},'Z'));
+        zTrace = traces(zIndex);
+        for compi = 1:length(corrseis)
+            trace.data = corrseis(compi).timeseries;
+            trace.network = network;
+            trace.station = station;
+            trace.channel = zTrace.channel;
+            trace.sacpz = zTrace.sacpz;
+            trace.sampleCount = length(corrseis(compi).timeseries);
+            trace.sampleRate = trace.sampleCount/EventDataLength;
+            trace.sensitivity = zTrace.sensitivity;
+            trace.sensitivityFrequency = zTrace.sensitivityFrequency;
+            trace.sensitivityUnits = zTrace.sensitivityUnits;
+            NewTrace = rm_resp(trace,eventid,LoPassCorner,nPoles,PZDir,1,length(corrseis));
+            if RespAfterFlag == 1
+                corrseis(compi).timeseries = NewTrace.data_cor;
+            end
+        end
+        
         corrected.params.f = f;
         corrected.params.tf_op = tf_op;
         corrected.params.eventid = eventid;
@@ -285,7 +326,7 @@ for ie = 1:length(event_filename)
         
         %station plots
         if isfigure_sta == 1;
-            spitplots_correctevent(dt,T1,T2,Zraw,H1raw,H2raw,Praw,taxisZ,eventid,netsta,corrseis)
+            spitplots_correctevent(dt,T1,T2,Zraw,H1raw,H2raw,Praw,taxisZ,eventid,netsta,corrseis,f,NFFT)
             if issavefigure==1
                 figure(101)
                 filename=sprintf('%s/%s_%s_originalseis',figoutpath,eventid,netsta);
@@ -299,6 +340,13 @@ for ie = 1:length(event_filename)
                     filename=sprintf('%s/%s_%s_corrseis_av',figoutpath,eventid,netsta);
                     print(gcf,'-dpng',filename)
                 end
+                figure(103)
+                filename=sprintf('%s/%s_%s_corrspectra',figoutpath,eventid,netsta);
+                print(gcf,'-dpng',filename)
+                
+                figure(104)
+                filename=sprintf('%s/%s_%s_seis+spectra',figoutpath,eventid,netsta);
+                print(gcf,'-dpng',filename)
             end
         end
 		%plot spectra against Peterson's low and high noise models
